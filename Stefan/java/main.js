d8c8a4d45bdf3d3be0f11abe1e78c8cdb9ab5c3f
102462-p <102462@glr.nl>
2024-10-09 14:13:59 +0000

    Gwerkt aan site

--- Stefan/java/main.js
+++ Stefan/java/main.js
@@ -1,11 +1,22 @@
 
-function KnopKlik(link){
-    let knop = document.getElementById("knop0");
-    knop.src = "assets/images/Button_1_Pressed.png";
-    setTimeout(GotoLink(knop), 300);
+function KnopKlik(id, link, index){
+    let knop;
+    switch(index){
+        case 0:
+            knop = document.getElementById(id);
+            knop.src = "assets/images/Button_1_Pressed.png";
+        case 1:
+            knop = document.getElementsByClassName("backhome");
+    }
+    setTimeout(GotoLink(link), 3000);
 }
 
-function GotoLink(knop){
-    //knop.src = "assets/images/Button_1.png";
-    window.location.replace("pages/History.html");
+function GotoLink(link){
+    window.location.href = link;
+}
+
+function Moveslide(dir){
+    console.log(dir);
+
+    
 }
